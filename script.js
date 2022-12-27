5a6a93c5074e034e312c88c61af2a6bebd22690b
sanaulla0 <vadapallisanaulla@gmail.com>
2022-12-27 15:27:21 +0530

    p2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let textbox = document.getElementById('evaluatedText');
+let count = document.getElementById('wordCount');
+
+function changecount(){
+
+ let calculate = textbox.value;
+ count.innerText = calculate.length;
+console.log(count);
+}
+
+
+textbox.addEventListener('keyup',changecount);
+
+
+
